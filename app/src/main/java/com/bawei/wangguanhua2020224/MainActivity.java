e04b874186978057750901687fcd917dcf82bf69
王冠华 <767986297@qq.com>
2020-02-24 13:22:11 +0800

    第一次提交

---
+++ app/src/main/java/com/bawei/wangguanhua2020224/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bawei.wangguanhua2020224;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * View层,加载页面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
